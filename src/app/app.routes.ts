e706005a7a114f0a029cb5f50059fcf9cc7ccb82
Raj Jirapure <vikichavan81@rediffmail.com>
2026-07-10 08:07:55 +0530

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,55 +12,55 @@ export const routes: Routes = [
     children: [
 
         // With lazy loading
-    //   {
-    //     path: 'dashboard',
-    //     loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
-    //   },
-    //   {
-    //     path: 'employees',
-    //     loadComponent: () => import('./features/employees/employee-list/employee-list').then((m) => m.EmployeeList),
-    //   },
-    //   {
-    //     path: 'products',
-    //     loadComponent: () => import('./features/products/product-list/product-list').then((m) => m.ProductList),
-    //   },
-    //   {
-    //     path: 'settings',
-    //     loadComponent: () => import('./features/settings/settings').then((m) => m.Settings),
-    //   },
-    //   {
-    //     path: '',
-    //     redirectTo: 'dashboard',
-    //     pathMatch: 'full'
-    //   }
-
-    // Without lazy loading
-
-    {
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+      {
+        path: 'employees',
+        loadComponent: () => import('./features/employees/employee-list/employee-list').then((m) => m.EmployeeList),
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./features/products/product-list/product-list').then((m) => m.ProductList),
+      },
+      {
+        path: 'settings',
+        loadComponent: () => import('./features/settings/settings').then((m) => m.Settings),
+      },
+      {
         path: '',
         redirectTo: 'dashboard',
         pathMatch: 'full'
-      },
-
-      {
-        path: 'dashboard',
-        component: Dashboard
-      },
-
-      {
-        path: 'employees',
-        component: EmployeeList
-      },
-
-      {
-        path: 'products',
-        component: ProductList
-      },
-
-      {
-        path: 'settings',
-        component: Settings
       }
+
+    // Without lazy loading
+
+    // {
+    //     path: '',
+    //     redirectTo: 'dashboard',
+    //     pathMatch: 'full'
+    //   },
+
+    //   {
+    //     path: 'dashboard',
+    //     component: Dashboard
+    //   },
+
+    //   {
+    //     path: 'employees',
+    //     component: EmployeeList
+    //   },
+
+    //   {
+    //     path: 'products',
+    //     component: ProductList
+    //   },
+
+    //   {
+    //     path: 'settings',
+    //     component: Settings
+    //   }
 
     ],
   },
